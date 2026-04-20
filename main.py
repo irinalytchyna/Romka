afcdbcc6096065db01e9f7dff734957c59773504
“Lutchyna” <lutchina@ukr.net>
2026-04-20 22:56:05 +0300

    Hii

---
+++ main.py
@@ -0,0 +1,14 @@
+from poems import VERSE1
+from poems import VERSE2
+from sign import HOMEWORK
+name = "Рома"
+homework = HOMEWORK.format(name=name)
+print(homework)
+wish = "Бажаю успіхів \n Щасливого програмування!!"
+print(wish)
+stars = "**************************************************************************"
+print(stars)
+print(VERSE1)
+print(stars)
+print(VERSE2)
+print(stars)
